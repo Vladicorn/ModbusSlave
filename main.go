32e2e1b8c65db6d9ab9a61f412a93900f9c37012
Vladimir aka UnicornOfLove <killmano32@yandex.ru>
2022-03-11 16:09:33 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 )
 
@@ -37,18 +38,19 @@ type MBAPHeader struct {
 }
 
 var regSlice = []byte{
-	0x18, 0x00,
-	0x12, 0xFF,
+	0x00, 0x0A,
+	0x00, 0xFF,
+	0x00, 0x01,
+	0x00, 0x03,
+	0x00, 0x04,
+	0x00, 0x00,
+	0x00, 0x05,
 	0x00, 0x00,
 	0x00, 0x00,
 	0x00, 0x00,
-	0x1F, 0x00,
-	0x09, 0x00,
-	0x01, 0x00,
-	0x1F, 0x00,
-	0x09, 0x00,
-	0x01, 0x00,
+	0x00, 0x00,
 }
+var slaveID uint8 = 2
 
 func main() {
 
@@ -69,9 +71,13 @@ func main() {
 		//считываем 12 байт в буффер
 		buf := make([]byte, 12)
 		if _, err := conn.Read(buf); err != nil {
-			fmt.Println(err)
-			return
-
+			if err == io.EOF {
+				fmt.Println("Connection lost.")
+				return
+			} else {
+				fmt.Println(err)
+				return
+			}
 		}
 		fmt.Println("Message Received:", buf)
 
@@ -101,30 +107,63 @@ func ReadHoldingRegister(data []byte) []byte {
 	telegram.MBAP.ProtocolID = binary.BigEndian.Uint16(buf[2:4])
 	telegram.MBAP.Length = binary.BigEndian.Uint16(buf[4:6])
 	telegram.MBAP.UnitID = buf[6]
+	if telegram.MBAP.UnitID == slaveID {
+		//обработка PDU
+		buf = make([]byte, telegram.MBAP.Length-1)
+		if _, err := conn.Read(buf); err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		telegram.PDU.FuncCode = buf[0]
+		telegram.PDU.FirstReg = binary.BigEndian.Uint16(buf[1:3])
+		telegram.PDU.CountReg = binary.BigEndian.Uint16(buf[3:5])
 
-	//обработка PDU
-	buf = make([]byte, telegram.MBAP.Length-1)
-	if _, err := conn.Read(buf); err != nil {
-		fmt.Println(err)
-		return nil
+		var telegDataMB TelegramAnsver
+		switch telegram.PDU.FuncCode {
+		case 3:
+			telegDataMB = ReadDataMBHolding(&telegram)
+		case 2:
+			fmt.Println("In process")
+		default:
+			fmt.Println("Unknow FuncCode")
+		}
+
+		var str []byte
+		str1 := make([]byte, 2)
+		binary.BigEndian.PutUint16(str1, telegDataMB.MBAP.TranID)
+		str2 := make([]byte, 2)
+		binary.BigEndian.PutUint16(str2, telegDataMB.MBAP.ProtocolID)
+		str3 := make([]byte, 2)
+		binary.BigEndian.PutUint16(str3, telegDataMB.MBAP.Length)
+		str4 := telegDataMB.MBAP.UnitID
+		str5 := telegDataMB.PDU.FuncCode
+		str6 := telegDataMB.PDU.CountReg
+
+		str = append(str, str1...)
+		str = append(str, str2...)
+		str = append(str, str3...)
+		str = append(str, str4)
+		str = append(str, str5)
+		str = append(str, str6)
+		str = append(str, telegDataMB.PDU.Data...)
+
+		return str
+	} else {
+		fmt.Println("Another SlaveID")
 	}
-	telegram.PDU.FuncCode = buf[0]
-	telegram.PDU.FirstReg = binary.BigEndian.Uint16(buf[1:3])
-	telegram.PDU.CountReg = binary.BigEndian.Uint16(buf[3:5])
 
-	str := RedHoldingInternal(&telegram)
-
-	return str
+	return nil
 }
 
 //Разбор дата поинт
-func RedHoldingInternal(teleg *Telegram) []byte {
-	//	fmt.Println(teleg)
+func ReadDataMBHolding(teleg *Telegram) TelegramAnsver {
+
 	slicereg := make([]byte, teleg.PDU.CountReg*2, 100)
 	var telega TelegramAnsver
 
 	j := 0
-	//проверка на количество считываний
+	//проверка на количество считываний не сделано
+
 	for i := teleg.PDU.FirstReg; i < teleg.PDU.CountReg*2; i++ {
 		slicereg[j] = regSlice[i]
 		j++
@@ -135,29 +174,7 @@ func RedHoldingInternal(teleg *Telegram) []byte {
 	telega.PDU.FuncCode = teleg.PDU.FuncCode
 	telega.PDU.CountReg = uint8(j)
 	telega.PDU.Data = slicereg
-	fmt.Println(telega.MBAP.Length)
-	var str []byte
-	str1 := make([]byte, 2, 2)
-	binary.BigEndian.PutUint16(str1, telega.MBAP.TranID)
-	str2 := make([]byte, 2, 2)
-	binary.BigEndian.PutUint16(str2, telega.MBAP.ProtocolID)
-	str3 := make([]byte, 2, 2)
-	binary.BigEndian.PutUint16(str3, telega.MBAP.Length)
-	var str4 byte
-	str4 = telega.MBAP.UnitID
-	var str5 byte
-	str5 = telega.PDU.FuncCode
-	var str6 byte
-	str6 = telega.PDU.CountReg
 
-	str = append(str, str1...)
-	str = append(str, str2...)
-	str = append(str, str3...)
-	str = append(str, str4)
-	str = append(str, str5)
-	str = append(str, str6)
-	str = append(str, telega.PDU.Data...)
-
-	return str
+	return telega
 
 }
